4f64c8ab661282902c14fe8ed17522d1c21d5710
ellienyc <148107694+ellienyc@users.noreply.github.com>
2025-04-23 21:14:07 -0400

    Weds 9pm: scripts/display issue

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # cuties-map
  
 In progress map, on all the pet sits I've done across NYC (mostly Manhattan and Queens)
+
+
+API endpoint: https://data.cityofnewyork.us/resource/hxx3-bwgv.json
--- scripts.js
+++ scripts.js
@@ -1,17 +1,35 @@
-
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWthcnBlIiwiYSI6ImNsbXM1a2c3MzBkZWEya24xdTB0dW5hZmEifQ.5vXxMn1kOMvdkUCdQfc0oQ';
 
-const petsMap = new mapboxgl.Map({
-    container: 'map-container', // container ID
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
     center: [-73.97604, 40.76906], // starting position [lng, lat]
-    zoom: 11.5, // zoom level
-    minZoom: 11,
+    zoom: 11.5, // opening zoom level
+    minZoom: 11, // constraining the Zoom level
     maxZoom: 15,
     //hash: true, // Enable hash in the URL to reflect the map's center and zoom level
-    bearing: 28.9, // bearing in degrees
-    pitch: 0, // pitch in degrees
+    bearing: 28.9, // switching to Manhattan north
+    pitch: 0, // pitch in degrees, so map is flat and not 3D
     style: 'mapbox://styles/mapbox/satellite-v9' // style URL
+});
+
+map.on('load', () => {
+    map.addSource('dogRuns', {
+        type: 'geojson',
+        data: 'https://data.cityofnewyork.us/resource/hxx3-bwgv.geojson'
+    });
+
+    map.addLayer({
+        'id': 'dogRuns',
+        'type': 'fill',
+        'source': 'dogRuns',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f08',
+            'fill-opacity': 1,
+            'outline-color': '#f08',
+            'outline-width': 200
+        }
+    });
 });
 
 const markerOptions = {
@@ -29,18 +47,17 @@ petData.forEach((record) => {
     let typeColor = '#ccc';
 
     if (record.type === 'cat') {
-        typeColor = 'brown'
+        typeColor = 'brown';
     }
     if (record.type === 'dog') {
-        typeColor = 'blue'
+        typeColor = 'blue';
     }
-    
-    
-new mapboxgl.Marker({
+
+    new mapboxgl.Marker({
         scale: 0.8,
         color: typeColor
     })
         .setLngLat([record.longitude, record.latitude])
         .setPopup(popup)
-        .addTo(petsMap)
-})
+        .addTo(map);
+});
